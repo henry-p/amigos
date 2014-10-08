70423af8be4d95a8aafc3ce5fbb42c1a16b5c769
Colin Shevlin <cwshevlin@gmail.com>
2014-10-08 16:02:58 -0500

    Model association testing is passing.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :creator, class_name: "User"
+  belongs_to :commentable, polymorphic: true
 end
--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,4 +1,7 @@
 class Snippet < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :creator, class_name: "User"
   belongs_to :group
+  has_many :comments, as: :commentable
+  
 end
+
