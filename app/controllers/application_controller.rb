4907e0e72f380aa435f87e3517334b0733b5d3a5
henry-p <henry.perschk@gmail.com>
2014-10-09 15:05:03 -0500

    header version 1. added .gitignore file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user, :logged_in?
+
   def logged_in?
   	 current_user ? true : false
   end
@@ -10,5 +12,4 @@ class ApplicationController < ActionController::Base
   def current_user
   	@user ||= User.find_by_id(session[:user_id])
   end
-
 end
