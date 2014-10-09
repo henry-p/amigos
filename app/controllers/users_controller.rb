d2fbfbb28a8336295071b06a1f5af69d984ddb92
Nigel Dunetts <ndunetts@gmail.com>
2014-10-09 16:29:45 -0500

    profile edit page functioning properly, moving on to styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,20 @@ class UsersController < ApplicationController
 		@user = User.second
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update_attributes(user_params)
+		redirect_to user_path
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :current_location)
+	end
+
 end
