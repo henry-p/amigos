86bad56b84bdce7f6e48162652102d8ff28d62d4
Nixon Lolila Ramin <nixonlr@gmail.com>
2014-10-09 16:28:40 -0500

    resolved merge conflicts in post after image-post changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,5 @@ class Post < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   belongs_to :group
   has_many :comments, as: :commentable
-<<<<<<< HEAD
-
-=======
   has_many :images, as: :imageable
->>>>>>> 23113cd40b68b29f2e994019330fac556d1b05c8
 end
