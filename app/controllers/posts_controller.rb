8bbc4f5dcb2c73515239d4af5cc1e6a790236427
Nixon Lolila Ramin <nixonlr@gmail.com>
2014-10-09 16:24:02 -0500

    pullin post changes from images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,49 @@
 class PostsController < ApplicationController
 
+	def index
+		@post = Post.all.order('updated_at')
+	end
 
-	def edit	
+	def show
+		@post = Post.find(params[:id])
+	end
 
+	def new
+		@group = Group.find(params[:group_id])
+		@user = User.find(params[:user_id])
+		@post = Post.new	
 	end
 
 
+	def create
+		# uploader = PictureUploader.new
+		# uploader.store!(params[:post][:picture])
+		post = Post.new(post_params)
+		image = Image.new(path: params[:post][:picture].path, imageable_type: "post", imageable_id: post.id)
+
+		if post.save
+			image.save
+			redirect_to group_user_post_path(params[:group_id],params[:user_id], post.id)
+		else
+			render 'form'
+		end
+	end
+
+	def edit	
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
+  mount_uploader :picture, PictureUploader
+  
   belongs_to :creator, class_name: "User"
   belongs_to :group
   has_many :comments, as: :commentable
+
 end
