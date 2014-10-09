74af6731ad6b73bceac107fcc5b226b1adf06937
Colin Shevlin <cwshevlin@gmail.com>
2014-10-09 17:05:42 -0500

    Added views for groups.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,22 +6,39 @@ class GroupsController < ApplicationController
 	end
 
 	def show
-		# if current_user
+		if current_user
 			@group = Group.find_by_id(params[:id])
 			@posts = @group.posts(order: "created_at DESC")
 			@snippets = @group.snippets(order: "created_at DESC")
 
 			@commentables = (@posts.to_a + @snippets.to_a).sort_by(&:created_at).reverse[0,5]
-		# else 
-		# 	redirect_to root_path
-		# end
+		else 
+			redirect_to root_path
+		end
 	end
 
 	def new
+		@group = Group.new
+	end
+
+	def create
+		@group = Group.new(group_params)
+		if @group.save
+			flash[:notice] = "Group successfully created."
+			redirect_to user_index_path
+		else
+			flash[:error] = "Group name already exists"
+			redirect_to new_group_path
+		end
 	end
 
 	def edit
+
 	end
 
+	private
 
+	def group_params
+    params.require(:group).permit(:name, :jefe_id)
+	end
 end
