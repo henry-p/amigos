0b89a084ce8bd34fe585707c6c231efd9520cd9d
henry-p <henry.perschk@gmail.com>
2014-10-08 20:06:07 -0500

    refined routes, added empty controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,31 @@
 Rails.application.routes.draw do
+  concern :commentable do
+    resources :comments, except: [:index, :show]
+  end
+  concern :imageable do
+    resources :images, except: [:index, :show]
+  end
+
   resources :users, only: [:new, :create]
-  resources :groups, except: :index do
+  resources :profile, controller: :users, as: :user, except: [:new, :create], concerns: :imageable
+  get "groups/:group_id/invite" => 'groups#send_invite', as: :send_invite
+  scope module: 'jefe' do
+    resources :groups, except: [:index, :new, :create, :show]
+  end
+  resources :groups, concerns: :imageable, only: [:show, :new, :create] do
     resources :posts, only: :index
     resources :snippets, only: :index
     resources :users, only: [:index, :show] do
-      resources :posts do
-        resources :comments, except: [:index, :show]
-      end
-      resources :snippets do
-        resources :comments, except: [:index, :show]
-      end
+      resources :posts, concerns: :commentable
+      resources :snippets, concerns: :commentable
     end
   end
-
-  get '/profile' => 'users#profile'
-  get '/profile/edit' => 'users#edit', as: :edit_user
-  patch '/profile' => 'users#update', as: :user
-  put '/profile' => 'users#update'
-  delete '/profile' => 'users#delete'
+  
+  # get '/profile' => 'users#profile'
+  # get '/profile/edit' => 'users#edit', as: :edit_user
+  # patch '/profile' => 'users#update', as: :user
+  # put '/profile' => 'users#update'
+  # delete '/profile' => 'users#delete'
 
 
 
