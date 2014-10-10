1d624963e18ad8927c0b769eea18ee7a842403b8
Colin Shevlin <cwshevlin@gmail.com>
2014-10-09 19:25:45 -0500

    Added some styling to the group page view.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,15 +6,15 @@ class GroupsController < ApplicationController
 	end
 
 	def show
-		if current_user
+		# if current_user
 			@group = Group.find_by_id(params[:id])
 			@posts = @group.posts(order: "created_at DESC")
 			@snippets = @group.snippets(order: "created_at DESC")
 
 			@commentables = (@posts.to_a + @snippets.to_a).sort_by(&:created_at).reverse[0,5]
-		else 
-			redirect_to root_path
-		end
+		# else 
+		# 	redirect_to root_path
+		# end
 	end
 
 	def new
