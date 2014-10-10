557dc1ebfbeb3e36a9e9dc08fa6adbbd8ede9066
henry-p <henry.perschk@gmail.com>
2014-10-09 20:31:06 -0500

    auth working (no ajax, no OAuth)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {sessions: 'sessions'}
   root to: 'home#index'
 
   concern :commentable do
--- db/schema.rb
+++ db/schema.rb
@@ -80,10 +80,6 @@ ActiveRecord::Schema.define(version: 20141009223047) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
