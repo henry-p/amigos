bada9adc3d0682a24dd993a6da0403b4b3b1c758
Nixon Lolila Ramin <nixonlr@gmail.com>
2014-10-10 16:22:11 -0500

    removed my additions to the user controller to prevent merge conflicts with nigel

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,27 +7,14 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		
 	end
 
 	def edit
 		@user = User.find(params[:id])
-		if @user.image
-			@image = @user.image
-		else
-			@user.image = Image.create
-			@image = @user.image
-		end
 	end
 
 	def update
 		@user = User.find(params[:id])
-		if params[:user][:picture]
-			image = Image.create(picture: params[:user][:picture])
-			@user.image = image
-			uploader = PictureUploader.new(image)
-			uploader.store!(image.picture)
-		end
 		@user.update_attributes(user_params)
 		redirect_to user_path
 	end
@@ -35,7 +22,7 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :email, :password, :current_location)
+		params.require(:user).permit(:first_name, :last_name, :current_location)
 	end
 
 end
