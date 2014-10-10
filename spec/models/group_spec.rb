7d696b2f78ddaf52f600c3cbe67630f864cd0005
Colin Shevlin <cwshevlin@gmail.com>
2014-10-10 14:08:02 -0500

    Added Group.create testing.

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe Group do
-  let(:group) { Group.create!(name: "Our cool group!") }
+  let(:group) { Group.create!(name: "Our cool group!", jefe_id: 1) }
+  let(:user) {User.create!(email: "test@test.com", password: "secure", first_name: "John", last_name: "Doe", current_location: "Chicago, IL")}
+
+
+  it "should have name and jefe" do
+    expect(group.name).to eq "Our cool group!"
+    expect(group.jefe.first_name).to eq "John"
+    expect(group.jefe_id).to eq user.id
+  end
 
   it "should be managed by a 'jefe'" do
     expect(group).to belong_to :jefe
