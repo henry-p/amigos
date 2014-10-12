68b9129d2b8b0cc9aae9aa0563dfc8ae28a072c5
Nixon Lolila Ramin <nixonlr@gmail.com>
2014-10-12 18:28:12 -0500

    about to pull seed changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,25 +4,18 @@ class CommentsController < ApplicationController
   
 	def new
 		@comment = Comment.new
-		@group = Group.find(params[:group_id])
-		@post_or_snippet_creator = User.find(params[:user_id])
-		if params[:post_id]
-			@post = Post.find(params[:post_id]) 
-		elsif params[:snippet_id]
-			@snippet = Snippet.find(params[:snippet_id])
-		end
 	end
 
 	def create
-		comment = Comment.new(comment_params)
-		comment.creator_id = current_user.id
+		@comment = Comment.new(comment_params)
+		@comment.creator_id = current_user.id
 		
 		if @post
-			@post.comments << comment
-			redirect_to [@group, @post_or_snippet_creator, @post]
+			@post.comments << @comment
+			redirect_to [@group, @post.creator, @post]
 		elsif @snippet
-			@snippet.comments << comment
-			redirect_to [@group, @post_or_snippet_creator, @snippet]
+			@snippet.comments << @comment
+			redirect_to [@group, @snippet.creator, @snippet]
 		end
 		
 	end
@@ -34,17 +27,23 @@ class CommentsController < ApplicationController
 	def update
 		@comment = Comment.find(params[:id])
 		@comment.update_attributes(comment_params)
-		redirect_to [@group, @post_or_snippet_creator, @snippet]
+		redirect_to (@post ? [@group, @post.creator, @post] : [@group, @snippet.creator, @snippet])
+	end
+
+	def destroy
+		@comment.destroy
+		redirect_to (@post ? [@group, @post.creator, @post] : [@group, @snippet.creator, @snippet])
 	end
 	
 	def group_user_and_comment_objects
+		@comment = Comment.find(params[:id]) unless %w[new create].include?(params[:action])
 		params[:snippet_id] ? @snippet = Snippet.find(params[:snippet_id]) : @post = Post.find(params[:post_id])
 		@group = Group.find(params[:group_id])
 		@post_or_snippet_creator = User.find(params[:user_id])
 	end
 
-	def is_current_user?
-		redirect_to group_user_snippets_path unless current_user.id == @user.id
+	def is_creator_of_comment?
+		redirect_to [@group, @post_or_snippet_creator, @snippet] unless current_user.id == @comment.creator_id
 	end
 
 	def login_required
